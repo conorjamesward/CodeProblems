c3c207f2093566801b61fb89420ec2591c8a251a
Conor Ward <sirconorjamesward@gmail.com>
2022-02-09 12:05:39 -0700

    optimum solution

--- twoSum.js
+++ twoSum.js
@@ -30,42 +30,59 @@
 
 // Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
+// const twoSum = (nums, target) => {
+//   const hashTable = {}
+
+//   const buildHash = () => {
+//     nums.forEach((num, i) => {
+//       if (hashTable[num]) {
+//         hashTable[num].push(i)
+//       } else {
+//         hashTable[num] = [i]
+//       }
+//     })
+//   }
+//   buildHash()
+
+//   const findSum = () => {
+//     let sum = null
+//     nums.forEach(num => {
+//       if (sum) { //found sum
+//         return
+//       } else if(num === target / 2){ //duplicate number case
+//         if (hashTable[num].length === 2){ //duplicate number special case
+//           sum = hashTable[num]
+//         }
+//       } else { //different numbers case
+//         const find = hashTable[num]
+//         if (find && hashTable[target - num]){
+//           sum = [...hashTable[target - num], ...find]
+//         }
+//       }
+//     })
+//     return sum
+//   }
+
+//   return findSum()
+// }
+
+
+//hashmap was fun to try, but not ultimately the best answer. The hints were misleading to me. The implementations of hasmap I found as examples were much larger then the below optimum solution
+
 const twoSum = (nums, target) => {
-  const hashTable = {}
-
-  const buildHash = () => {
-    nums.forEach((num, i) => {
-      if (hashTable[num]) {
-        hashTable[num].push(i)
-      } else {
-        hashTable[num] = [i]
+  let map = {}; //hash
+  for(let x = 0; x < nums.length; x++){ //iterate
+      let sum = target - nums[x]; // product
+      //map of product !== undefiend && map of product does not = x
+      //return map of [product and x]
+      if(map[sum] !== undefined && map[sum] !== x){
+          return [map[sum], x];
       }
-    })
+      map[nums[x]] = x; //add to map
   }
-  buildHash()
-
-  const findSum = () => {
-    let sum = null
-    nums.forEach(num => {
-      if (sum) { //found sum
-        return
-      } else if(num === target / 2){ //duplicate number case
-        if (hashTable[num].length === 2){ //duplicate number special case
-          sum = hashTable[num]
-        }
-      } else if (num <= target){ //different numbers case
-        const find = hashTable[num]
-        if (find && hashTable[target - num]){
-          sum = [...hashTable[target - num], ...find]
-        }
-      }
-    })
-    return sum
-  }
-
-  return findSum()
-}
+};
 
 console.log(twoSum([2,7,11,15],9))
 console.log(twoSum([3,2,4], 6))
 console.log(twoSum([3,3], 6))
+console.log(twoSum([-1,-2,-3,-4,-5], -8))
