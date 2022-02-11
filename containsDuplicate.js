cc90a96ba53861f999f6f41630748790c685d114
Conor Ward <sirconorjamesward@gmail.com>
2022-02-11 08:45:52 -0700

    contains duplicate

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -9,4 +9,14 @@ const containsDuplicate = nums => {
   }) !== undefined
 }
 
+//optimal runtime solution from leetcode
+// var containsDuplicate = function(nums) {
+//   let hm=new Map();
+//   for(let num of nums){
+//       if(hm.has(num)) return true;
+//       hm.set(num,1);
+//   }
+//   return false;
+// };
+
 console.log(containsDuplicate([0,4,5,0,3,6]))
